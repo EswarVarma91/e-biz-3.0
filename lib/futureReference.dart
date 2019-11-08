d7f7b5d37b6174a958663490cc41f7885fbfd2fb
eswarvarma-dev <54401425+eswarvarma-dev@users.noreply.github.com>
2019-11-08 20:43:50 +0530

    observation

--- lib/futureReference.dart
+++ lib/futureReference.dart
@@ -1,1 +1,5 @@
 //static  var now = DateTime.now(); DateFormat("yyyy-MM-dd hh:mm:ss").format(now)
+
+// https://flatteredwithflutter.com/flutter-and-services/
+// https://stackoverflow.com/questions/50106844/how-to-create-a-service-in-flutter-to-make-an-app-to-run-always-in-background?rq=1
+// https://medium.com/stuart-engineering/%EF%B8%8F-the-tricky-task-of-keeping-flutter-running-on-android-2d51bbc60882
