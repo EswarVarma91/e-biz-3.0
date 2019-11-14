7ea56fdbad1b455a22f115d5037a6c0d7366b5d2
eswarvarma-dev <54401425+eswarvarma-dev@users.noreply.github.com>
2019-11-14 18:28:32 +0530

    unwanted services commented

--- lib/myConfig/ServicesApi.dart
+++ lib/myConfig/ServicesApi.dart
@@ -2,26 +2,33 @@ class ServicesApi{
 
   //========= New DataBase ebizdb=========//
 
-  // static String basic_url = "http://192.168.2.5:8383/";
+  // static String basic_url = "http://192.168.2.5:8383/"; //Live
 
 
-  static String basic_url ="http://192.168.2.3:8080/";
+  static String basic_url ="http://192.168.2.3:8080/";//test
+
   static String new_login_url = basic_url + "global.service/validate/user";
 
   static String global_Service = "global.service/global/";
   static String hrms_Service = "hrms.service/hrms/";
   static String att_Service = "att.service/hrms/";
   static String travel_Service = "travel.service/travel/";
+  static String crm='crm.service/';
 
+  //CRM Sales Lead
+  static String Pending_Url = eagle_emp_main + crm +"crm/get/data";
+  static String Sales_Insert_Url= eagle_emp_main + crm +'crm/sales/request/save';
+
+  //
   static String getData = basic_url + global_Service + "getAnyGlobalDataForMobile";
   static String updateData = basic_url + global_Service + "updateAnyGlobalDataForMobile";
 
-  //leaves
+  //Leaves
   static String insertLeave = basic_url + hrms_Service +"saveEmpLeave";
   static String ChangeLeaveStatus = basic_url + hrms_Service + "update/leave/status";
   static String checkLeaveStatus = basic_url + hrms_Service + "get/hruser/data";
   
-  //permissions
+  //Permissions
   static String insertPermission = basic_url + hrms_Service +"saveEmpPermission";
   static String ChangePermissionStatus = basic_url + hrms_Service + "update/Permission/status";
   static String getLeaves = basic_url + att_Service + "attendance/get/user/leaves?id=";
@@ -30,12 +37,12 @@ class ServicesApi{
   static String saveDayPlan = basic_url + hrms_Service + "saveDayPlan";
 
   //get Travel Request List
-  static String getTravelList= basic_url + travel_Service + "get/travel/request";
+  // static String getTravelList= basic_url + travel_Service + "get/travel/request";
 
 
 
   //not working not supported error
-  static String leavebyUserId = "http://192.168.2.5:8383/hrms.service/hrms/getEmpLeaveByUserId?id=";
+  // static String leavebyUserId = "http://192.168.2.5:8383/hrms.service/hrms/getEmpLeaveByUserId?id=";
 
 
 
@@ -65,9 +72,8 @@ class ServicesApi{
   static String eagle_emp_main= 'http://192.168.2.3:8080/'; // testing
 
   static String Login_Url= eagle_emp_main + "global.service/global/get/any/data";
-  static String crm='crm.service/';
-  static String Pending_Url = eagle_emp_main + crm +"crm/get/data";
-  static String Sales_Insert_Url= eagle_emp_main + crm +'crm/sales/request/save';
+  
+  
 
 
 
