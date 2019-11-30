d9097c169e409fb1d19779135352bc350f5641f7
eswarvarma-dev <54401425+eswarvarma-dev@users.noreply.github.com>
2019-11-30 19:46:03 +0530

    firebase {} removed for message

--- lib/activity/Login.dart
+++ lib/activity/Login.dart
@@ -50,7 +50,7 @@ class _LoginState extends State<Login> {
 
     _firebaseMessaging.configure(
       onMessage: (Map<String, dynamic> message) {
-        print('on message ${message}');
+        print('on message $message');
         // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
         displayNotification(message);
         // _showItemDialog(message);
