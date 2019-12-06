e033814f92e88ce14c220588149795addfb7d9a6
eswarvarma-dev <54401425+eswarvarma-dev@users.noreply.github.com>
2019-12-06 17:58:43 +0530

    appcenter uncaught exceptions log

--- android/app/src/main/java/ebiz/lotus/administrator/eaglebiz/MainActivity.java
+++ android/app/src/main/java/ebiz/lotus/administrator/eaglebiz/MainActivity.java
@@ -3,11 +3,16 @@ package ebiz.lotus.administrator.eaglebiz;
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
-  }
+    AppCenter.start(getApplication(), "f56ca720-90e4-4964-92a4-10c291e68a86",
+                  Analytics.class, Crashes.class);
+    }
 }
