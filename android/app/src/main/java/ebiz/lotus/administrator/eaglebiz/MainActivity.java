eff8f11a0c43e1be1e4b6ca0e4006657079c45eb
eswarvarma-dev <54401425+eswarvarma-dev@users.noreply.github.com>
2019-12-07 15:47:37 +0530

    native bg service completed pending communication with flutter

--- android/app/src/main/java/ebiz/lotus/administrator/eaglebiz/MainActivity.java
+++ android/app/src/main/java/ebiz/lotus/administrator/eaglebiz/MainActivity.java
@@ -3,55 +3,29 @@ package ebiz.lotus.administrator.eaglebiz;
 import android.os.Bundle;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import io.flutter.app.FlutterActivity;
-import android.widget.Toast;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
 import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends FlutterActivity {
-  AlarmManager alarmManager;
-  PendingIntent pendingIntent;
-  BroadcastReceiver mReceiver;
-  int counter=0;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
     AppCenter.start(getApplication(), "f56ca720-90e4-4964-92a4-10c291e68a86", Analytics.class, Crashes.class);
-    RegisterAlarmBroadcast();
-    alarmManager.setRepeating(AlarmManger.RTC_WAKEUP, System.currentTimeMillis(), 15 * 60 * 1000, pendingIntent);
+    startAlert();
   }
 
-  private void RegisterAlarmBroadcast() {
-    mReceiver = new BroadcastReceiver() {
-      // private static final String TAG = "Alarm Example Receiver";
-      @Override
-      public void onReceive(Context context, Intent intent) {
-        counter++;
-        Toast.makeText(context, "Background Service Started "+counter, Toast.LENGTH_LONG).show();
-      }
-    };
+  public void startAlert() {
+    Intent intent = new Intent(this, MyBroadcastReceiver.class);
+    PendingIntent pendingIntent = PendingIntent.getBroadcast(this.getApplicationContext(), 234, intent, 0);
+    AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+    alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(),1*60*1000, pendingIntent);
 
-    registerReceiver(mReceiver, new IntentFilter("sample"));
-    pendingIntent = PendingIntent.getBroadcast(this, 0, new Intent("sample"), 0);
-    alarmManager = (AlarmManager) (this.getSystemService(Context.ALARM_SERVICE));
   }
 
-  private void UnregisterAlarmBroadcast() {
-    alarmManager.cancel(pendingIntent);
-    getBaseContext().unregisterReceiver(mReceiver);
-  }
-
-  @Override
-  protected void onDestroy() {
-    unregisterReceiver(mReceiver);
-    super.onDestroy();
-  }
 }
--- android/app/src/main/java/ebiz/lotus/administrator/eaglebiz/MyBroadcastReceiver.java
+++ android/app/src/main/java/ebiz/lotus/administrator/eaglebiz/MyBroadcastReceiver.java
@@ -1,4 +1,18 @@
 package ebiz.lotus.administrator.eaglebiz;
 
-public class MyBroadcastReceiver {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.util.Log;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+    MediaPlayer mp;
+    @Override
+    public void onReceive(Context context, Intent intent) {
+//        mp=MediaPlayer.create(context, R.raw.alarm);
+//        mp.start();
+//        Toast.makeText(context, "Alarm", Toast.LENGTH_LONG).show();
+        Log.d("check : ","bg service ");
+    }
 }
