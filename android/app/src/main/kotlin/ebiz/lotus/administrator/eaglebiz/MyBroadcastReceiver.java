9d72afb5a9897a6946634f02c8971eb86e127f74
eswarvarma-dev <54401425+eswarvarma-dev@users.noreply.github.com>
2020-01-08 17:36:03 +0530

    location testing

--- android/app/src/main/kotlin/ebiz/lotus/administrator/eaglebiz/MyBroadcastReceiver.java
+++ android/app/src/main/kotlin/ebiz/lotus/administrator/eaglebiz/MyBroadcastReceiver.java
@@ -59,8 +59,8 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // mp = MediaPlayer.create(context, R.raw.alarm);
-        // mp.start();
+        mp = MediaPlayer.create(context, R.raw.alarm);
+        mp.start();
 //        new NukeSSLCerts().nuke();
 //        HttpsTrustManager.allowAllSSL();
         simpleLocation = new SimpleLocation(context);
--- lib/myConfig/ServicesApi.dart
+++ lib/myConfig/ServicesApi.dart
@@ -4,12 +4,12 @@ class ServicesApi {
   static String fcm_Send = "https://fcm.googleapis.com/fcm/send";
   static String FCM_KEY = "AAAASa5BYMA:APA91bETvZbudrnnPOBaf6h395TagXNekYBGMtjJDycZ42_3mjxWEWccnpWpou3hBGsbQQbaSiJTCH1pA5VRo-2QsClDxq7V1wpOw1dNtu8V0Z5H66-NQpwGGnUSnXRlgzTGi584Z0WN";
 
-  static String basic_url = "http://192.168.2.5:8383/"; //dev
-  // static String basic_url = "http://192.168.2.3:8080/"; //test
+  // static String basic_url = "http://192.168.2.5:8383/"; //dev
+  static String basic_url = "http://192.168.2.3:8080/"; //test
   // static String basic_url = "http://www.e-biz.in:8083/"; //global
 
-  static String hrms_Service = "http://192.168.2.5:8383/hrms.service/"; //dev
-  // static String hrms_Service="http://192.168.2.3:8080/hrms.service/"; //test
+  // static String hrms_Service = "http://192.168.2.5:8383/hrms.service/"; //dev
+  static String hrms_Service="http://192.168.2.3:8080/hrms.service/"; //test
   // static String hrms_Service = "http://www.e-biz.in:8083/hrms.service/"; //global
 
 
