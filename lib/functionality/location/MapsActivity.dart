739d94c7c36e33f68c11b70a995f163bd9542851
eswarvarma-dev <54401425+eswarvarma-dev@users.noreply.github.com>
2020-01-20 19:21:01 +0530

    width of card

--- lib/functionality/location/MapsActivity.dart
+++ lib/functionality/location/MapsActivity.dart
@@ -685,16 +685,6 @@ class _ViewMapState extends State<MapsActivity> {
     super.dispose();
   }
 
-  // Future<Uint8List> getBytesFromAsset(String path, int width) async {
-  //   ByteData data = await rootBundle.load(path);
-  //   ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
-  //       targetWidth: width);
-  //   ui.FrameInfo fi = await codec.getNextFrame();
-  //   return (await fi.image.toByteData(format: ui.ImageByteFormat.png))
-  //       .buffer
-  //       .asUint8List();
-  // }
-
   Future<Uint8List> getBytesFromCanvas(int width, int height, int i) async {
     final ui.PictureRecorder pictureRecorder = ui.PictureRecorder();
     final Canvas canvas = Canvas(pictureRecorder);
@@ -718,8 +708,8 @@ class _ViewMapState extends State<MapsActivity> {
     painter.layout();
     painter.paint(
         canvas,
-        Offset((width * 0.5) - painter.width * 0.5,
-            (height * 0.5) - painter.height * 0.5));
+        Offset((width * 0.2) - painter.width * 0.2,
+            (height * 0.2) - painter.height * 0.2));
     final img = await pictureRecorder.endRecording().toImage(width, height);
     final data = await img.toByteData(format: ui.ImageByteFormat.png);
 
